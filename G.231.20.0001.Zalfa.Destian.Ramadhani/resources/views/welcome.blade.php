f5998106039b659c04de60fb1e402fd9e75a90a7
Zalfa Destian Ramadhani <voidoofrzzz@gmail.com>
2023-05-07 11:28:47 +0700

    Zalfa Destian Ramadhani G.211.20.0076 Praktikum 1 Rekayasa Website

---
+++ G.231.20.0001.Zalfa.Destian.Ramadhani/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Web Zalfa Destian Ramadhani NIM G.211.20.0076</title>
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<h1>Zalfa Destian Ramadhani</h1>
+		<h3>NIM G.211.20.0076</h3>
+        <h4>Kelas A2</h4>
+        <h3>Praktikum 1</h3>
+        <h4>MATA KULIAH REKAYASA WEBSITE</h4>
+	</div>
+
+	<script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
+</body>
+</html>
